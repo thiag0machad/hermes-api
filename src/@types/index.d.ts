03cea141bb4b9a504e28bc7aa390664553e03588
Thiago Machado <thcosta0@hotmail.com>
2021-11-04 16:43:30 -0300

    Ajustando configuração ESLint

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,4 +1,4 @@
-export interface Email {
+interface Email {
   name: string
   email: string
   body: string
--- src/models/contact.ts
+++ src/models/contact.ts
@@ -1,11 +1,5 @@
 import { transporter } from '../configs/email'
 
-interface Email {
-  name: string
-  email: string
-  body: string
-}
-
 export const contactModel = async ({ name, email, body }: Email) => {
   try {
     return transporter.sendMail({
