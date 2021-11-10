c74d9b420171cd58e5448185396af4d5e81db0ab
Thiago Machado <thcosta0@hotmail.com>
2021-11-10 10:44:51 -0300

    Selenium configurado

--- src/models/BrlModel.ts
+++ src/models/BrlModel.ts
@@ -6,10 +6,20 @@ export const convertUsdToBrlModel = async (value: string) => {
     const driver = await new Builder().forBrowser('chrome').build()
     await driver.get('https://dolarhoje.com/')
     await driver.manage().window().setRect({ width: 1456, height: 876 })
+
     const usd = await driver
       .findElement(By.id('nacional'))
       .getAttribute('value')
-    const total = parseFloat(usd) * parseFloat(value)
+
+    const usdReplaced = usd.replace(',', '.')
+
+    const total = {
+      Dolar: usdReplaced,
+      Real: value,
+      Convertido: `R$ ${(parseFloat(usdReplaced) * parseFloat(value)).toFixed(
+        2
+      )}`
+    }
 
     return total
   } catch (err) {
