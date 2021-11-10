daf133cb34e71421e415d79b91cf01468a9bc3c2
thiago0x01 <thcosta0@hotmail.com>
2021-11-10 10:49:59 -0300

    Selenium configurado

--- src/models/BrlModel.ts
+++ src/models/BrlModel.ts
@@ -1,11 +1,14 @@
 import { Builder, By } from 'selenium-webdriver'
+import chrome from 'selenium-webdriver/chrome'
 import 'chromedriver'
 
 export const convertUsdToBrlModel = async (value: string) => {
   try {
-    const driver = await new Builder().forBrowser('chrome').build()
+    const driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(new chrome.Options().headless())
+      .build()
     await driver.get('https://dolarhoje.com/')
-    await driver.manage().window().setRect({ width: 1456, height: 876 })
 
     const usd = await driver
       .findElement(By.id('nacional'))
