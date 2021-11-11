31f26abc777db8cfdd7f33ac179e6fde8bc6f28f
thiago0x01 <thcosta0@hotmail.com>
2021-11-11 10:53:51 -0300

    Cors adicionado no app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(cors)
 
 app.get('/', (req, res) => res.send('Bem Vindo!'))
 
